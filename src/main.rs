8db67136ac872fa99c5847269591c57ba69e50eb
MasonStooksbury <masonstooksbury@gmail.com>
2025-06-27 00:06:38 -0500

    Working on stuff

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,61 @@
 use reqwest::{Client, Error};
 use dotenv::dotenv;
 use std::env;
+use serde_json::Value;
+use std::collections::HashMap;
+
+
+#[derive(Debug)]
+struct ForecastPeriod {
+    number: u8,
+    name: String,
+    detailedForecast: String
+}
+
+
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
 
     dotenv().ok();
 
-    let STATE = env::var("STATE");
-    let ZONE = env::var("ZONE");
+    let STATE = env::var("STATE").unwrap();
+    let ZONE = env::var("ZONE").unwrap();
 
+    let url = format!("https://api.weather.gov/zones/{}/{}/forecast", STATE.to_string(), ZONE.to_string());
 
+    let user_agent = "Mozilla/5.0 (X11; Linux x86_64; rv:139.0) Gecko/20100101 Firefox/139.0";
+        
     println!("{:?}", STATE);
     println!("{:?}", ZONE);
+    println!("{:?}", url);
 
     let client = Client::builder()
-        .user_agent("Mozilla/5.0 (X11; Linux x86_64; rv:139.0) Gecko/20100101 Firefox/139.0")
+        .user_agent(user_agent)
         .build()?;
 
-
-    println!("{:?}", format!("https://api.weather.gov/zones/{:?}/{:?}/forecast", Some(STATE.clone()), Some(ZONE.clone())));
     let response = client
-        .get(format!("https://api.weather.gov/zones/{:?}/{:?}/forecast", STATE, ZONE))
+        .get(url)
         .send()
-        .await
-        .unwrap();
+        .await?;
 
-    let json: serde_json::Value = response.json().await.unwrap();
+    //let json2: Vec<ForecastPeriod> = serde_json.from_str(response.text().await)?;
+    let json: Vec<HashMap<String, Value>> = serde_json::from_str(&response.text().await.unwrap()).unwrap();
 
+    //response.json().await.unwrap();
+    //let periods = json.properties.clone();
+    println!("{:#?}", json);
+    //let periods: Vec<ForecastPeriod> = Vec::new();
+    //assert!(periods.is_array());
+    //for i in periods {
+        //periods.push(i);
+    //    println!("{:#?}", i);
+    //}
+
+    //println!("wooo: {:#?}", json2["properties"]);
     //println!("Status: {}", response.status());
     //println!("Body: {}", response.text().await?);
-    println!("Weee: {}", json["properties"]);
+    //println!("Weee: {:#?}", json["properties"]);
 
     Ok(())
 }
